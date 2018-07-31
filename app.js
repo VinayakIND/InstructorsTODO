e353c3ce1a10685e6b2cd69352bf92a690b4c974
Vinayak Mahajan <200353809@student.georgianc.on.ca>
2018-07-31 10:23:28 -0400

    Twitter OAuth setup done.

--- app.js
+++ app.js
@@ -100,3 +100,68 @@ app.use(function(err, req, res, next) {
 
 //Finally export the app so other files can use it.
 module.exports = app;
+
+/*Twitter OAuth */
+/* Use module */
+var OAuth = require('oauth').OAuth
+  , oauth = new OAuth(
+      "https://api.twitter.com/oauth/request_token",
+      "https://api.twitter.com/oauth/access_token",
+      process.env.twitter_consumer_key,
+      process.env.twitter_consumer_secret,
+      "1.0",
+      "https://instructors-to-do.herokuapp.com/auth/twitter/callback",
+      "HMAC-SHA1"
+    );
+
+// /auth/twitter
+app.get('/auth/twitter', function(req, res) {
+ 
+    oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+      if (error) {
+        console.log(error);
+        res.send("Authentication Failed!");
+      }
+      else {
+        req.session.oauth = {
+        token: process.env.oauth_token,
+        token_secret: process.env.oauth_token_secret
+        };
+        console.log(req.session.oauth);
+        res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+      }
+    });
+     
+});
+
+//redirect to the instructorsTODO site.	
+app.get('/auth/twitter/callback', function(req, res, next) {
+ 
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    var oauth_data = req.session.oauth;
+ 
+    oauth.getOAuthAccessToken(
+      oauth_data.token,
+      oauth_data.token_secret,
+      oauth_data.verifier,
+      function(error, oauth_access_token, oauth_access_token_secret, results) {
+        if (error) {
+          console.log(error);
+          res.send("Authentication Failure!");
+        }
+        else {
+          req.session.oauth.access_token = oauth_access_token;
+          req.session.oauth.access_token_secret = oauth_access_token_secret;
+          console.log(results, req.session.oauth);
+          res.send("Authentication Successful");
+          res.redirect('/instructor'); 
+        }
+      }
+    );
+  }
+  else {
+    res.redirect('/login'); // Redirect to login page
+  }
+ 
+});
